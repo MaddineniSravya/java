2ff76cdf38606ae882b6fa1763e011c59247e6c4
MaddineniSravya <msravya6969@gmail.com>
2026-05-28 13:43:56 +0530

    Delete Assigmnent-3 directory

--- Assigmnent-3/OddNumbers.java
+++
@@ -1,9 +0,0 @@
-public class OddNumbers {
-    public static void main(String[] args) {
-        System.out.println("Pattern: 1 3 5 7 9 ... till 100");
-        for (int i = 1; i <= 100; i += 2) {
-            System.out.print(i + "\t");
-        }
-        System.out.println(); // For a new line at the end
-    }
-}
